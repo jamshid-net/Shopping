835860a8ccfa2f7cf5c93bbc6470208fac997520
akramov_developer <104636440+akramovdeveloper@users.noreply.github.com>
2023-06-01 23:38:42 +0300

    Update CustomResourceFilterAttribute.cs (#4)

--- ProductWebApi/Attributes/CustomResourceFilterAttribute.cs
+++ ProductWebApi/Attributes/CustomResourceFilterAttribute.cs
@@ -2,5 +2,14 @@
 {
     public class CustomResourceFilterAttribute : Attribute 
     {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
